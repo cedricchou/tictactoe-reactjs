f3a87ec5d307db2e62ce0608430fa6b03ee588f4
Cedric Chou <cdric.chou@gmail.com>
2018-05-31 17:05:28 -0700

    prevent state change when a X or O is already occupying the cell

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,41 @@
-import React, {Component} from 'react';
-import './app.css';
+import React, { Component } from "react";
+import "./app.css";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sign: "X",
+      array: Array(9).fill("")
+    };
+  }
 
-    constructor() {
-        super ();
-        this.state = {
-            sign: "X"
-        }
+  clicked(event) {
+    if (this.state.array[event.target.dataset.cell] === "") {
+      this.state.array[event.target.dataset.cell] = this.state.sign;
+      event.target.innerHTML = this.state.sign;
+      this.setState({
+        sign: this.state.sign === "X" ? "O" : "X",
+        array: this.state.array
+      });
     }
-    
-    clicked(event) {
-        event.target.innerHTML = this.state.sign;
-        this.setState({
-            sign: this.state.sign === "X" ? "O" : "X"
-        })
-    }
+  }
 
-    render() {
-        return (
-            <div id="main-div">
-                <div id="game-board" onClick={(e)=>this.clicked(e)}>
-                    <div className="cell" id="cell1"></div>
-                    <div className="cell" id="cell2"></div>
-                    <div className="cell" id="cell3"></div>
-                    <div className="cell" id="cell4"></div>
-                    <div className="cell" id="cell5"></div>
-                    <div className="cell" id="cell6"></div>
-                    <div className="cell" id="cell7"></div>
-                    <div className="cell" id="cell8"></div>
-                    <div className="cell" id="cell9"></div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main-div">
+        <div id="game-board" onClick={e => this.clicked(e)}>
+          <div className="cell" data-cell="0" id="cell1" />
+          <div className="cell" data-cell="1" id="cell2" />
+          <div className="cell" data-cell="2" id="cell3" />
+          <div className="cell" data-cell="3" id="cell4" />
+          <div className="cell" data-cell="4" id="cell5" />
+          <div className="cell" data-cell="5" id="cell6" />
+          <div className="cell" data-cell="6" id="cell7" />
+          <div className="cell" data-cell="7" id="cell8" />
+          <div className="cell" data-cell="8" id="cell9" />
+        </div>
+      </div>
+    );
+  }
 }
