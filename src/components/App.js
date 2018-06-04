90ea40d8d53221095179834b9eb32d5d35aa46f3
Cedric Chou <cdric.chou@gmail.com>
2018-06-04 16:58:29 -0700

    draw added, need to add feature to freeze the game when winner decided

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,9 @@ export default class App extends Component {
       sign: "X",
       array: Array(9).fill(""),
       endOfGame: false,
-      winner: undefined
+      winner: undefined,
+      moves: 0,
+      gameStatus: ""
     };
   }
 
@@ -18,22 +20,29 @@ export default class App extends Component {
       event.target.innerHTML = this.state.sign;
       this.setState({
         sign: this.state.sign === "X" ? "O" : "X",
-        array: this.state.array
+        array: this.state.array,
+        moves: this.state.moves + 1
       });
     }
     const result = this.winner();
     if (result === "X") {
       this.setState({
         endOfGame: true,
-        winner: "X"
+        winner: "X",
+        gameStatus: "The winner is X"
       });
-      console.log(`The winner is X`);
     } else if (result === "X") {
       this.setState({
         endOfGame: true,
-        winner: "O"
+        winner: "O",
+        gameStatus: "The winner is O"
       });
-      console.log(`The winner is O`);
+    } else if (result === "draw") {
+      this.setState({
+        endOfGame: true,
+        winner: "draw",
+        gameStatus: "No winner, the game is a draw"
+      });
     }
   }
 
@@ -53,24 +62,32 @@ export default class App extends Component {
       if (
         array[winningCombo[i][0]] === array[winningCombo[i][1]] &&
         array[winningCombo[i][1]] === array[winningCombo[i][2]]
-      )
+      ) {
         return array[winningCombo[i][0]];
+      }
+
+      if (this.state.moves === 8) {
+        return "draw";
+      }
     }
   }
 
   render() {
     return (
       <div id="main-div">
-        <div id="game-board" onClick={e => this.clicked(e)}>
-          <div className="cell" data-cell="0" id="cell1" />
-          <div className="cell" data-cell="1" id="cell2" />
-          <div className="cell" data-cell="2" id="cell3" />
-          <div className="cell" data-cell="3" id="cell4" />
-          <div className="cell" data-cell="4" id="cell5" />
-          <div className="cell" data-cell="5" id="cell6" />
-          <div className="cell" data-cell="6" id="cell7" />
-          <div className="cell" data-cell="7" id="cell8" />
-          <div className="cell" data-cell="8" id="cell9" />
+        <div id="game-status">
+          {this.state.gameStatus}
+          <div id="game-board" onClick={e => this.clicked(e)}>
+            <div className="cell" data-cell="0" id="cell1" />
+            <div className="cell" data-cell="1" id="cell2" />
+            <div className="cell" data-cell="2" id="cell3" />
+            <div className="cell" data-cell="3" id="cell4" />
+            <div className="cell" data-cell="4" id="cell5" />
+            <div className="cell" data-cell="5" id="cell6" />
+            <div className="cell" data-cell="6" id="cell7" />
+            <div className="cell" data-cell="7" id="cell8" />
+            <div className="cell" data-cell="8" id="cell9" />
+          </div>
         </div>
       </div>
     );
