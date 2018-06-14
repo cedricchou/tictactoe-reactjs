bc8e4bf2408f8f039227feab44a9b44a8adab8b8
Cedric Chou <cdric.chou@gmail.com>
2018-06-13 17:04:37 -0700

    fixed bugs of winning conditions

--- src/components/App.js
+++ src/components/App.js
@@ -15,43 +15,26 @@ export default class App extends Component {
     this.restart = this.restart.bind(this);
   }
 
-  winner() {
-    const winningCombo = [
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    const array = this.state.array;
-    for (let i = 0; i < winningCombo.length; i++) {
-      if (
-        array[winningCombo[i][0]] === array[winningCombo[i][1]] &&
-        array[winningCombo[i][1]] === array[winningCombo[i][2]]
-      ) {
-        return array[winningCombo[i][0]];
-      }
-    }
-  }
-
   clicked(event) {
     if (this.state.array[event.target.dataset.cell] === null) {
-      this.state.array[event.target.dataset.cell] = this.state.sign;
+      const newArr = [...this.state.array];
+      newArr[event.target.dataset.cell] = this.state.sign;
       event.target.innerHTML = this.state.sign;
-      this.setState({
-        sign: this.state.sign === "X" ? "O" : "X",
-        array: this.state.array,
-        moves: this.state.moves + 1
-      });
-      console.log(this.state.moves);
+      this.setState(
+        {
+          array: newArr,
+          sign: this.state.sign === "X" ? "O" : "X",
+          moves: this.state.moves + 1
+        },
+        this.checkWinState.bind(this)
+      );
       if (this.state.endOfGame !== false) {
         event.target.innerHTML = null;
       }
     }
+  }
 
+  checkWinState() {
     const result = this.winner();
     if (result === "X") {
       this.setState({
@@ -72,6 +55,30 @@ export default class App extends Component {
         gameStatus: "The game is a draw"
       });
     }
+  }
+
+  winner() {
+    const winningCombo = [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    const array = this.state.array;
+    let win;
+    winningCombo.forEach(combo => {
+      if (
+        array[combo[0]] === array[combo[1]] &&
+        array[combo[1]] === array[combo[2]]
+      ) {
+        win = array[combo[0]];
+      }
+    });
+    return win;
   }
 
   restart() {
